155d9fba212be989d10bdc7974b67a83e173b6ad
davirshark <davirodrigo2524@gmail.com>
2021-07-16 09:39:06 -0300

    aula grid content outras alterações necessárias no textcomponent

---
+++ src/components/GridContet/mock.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Os grandes do xadrez',
+  html: `<p><strong>do Xadrez do brasil</p>
+  <ol>
+     <li>Krikor Mekhitarian</li>
+     <li>Alexandr Fier</li>
+  </ol>
+  <blockquote><p></p></blockquote>
+  <h3>Xadrez Mundial</h3>
+  <ul>
+     <li>Magnus Carlsen</li>
+     <li>Daniil Dubov</li>
+  </ul>
+  <pre><code>
+  </code></pre>`,
+  background: false,
+};
